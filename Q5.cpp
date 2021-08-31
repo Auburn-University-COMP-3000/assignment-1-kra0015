f1e500651b34d1644bd8166d693421486f645d48
kra0015 <89806584+kra0015@users.noreply.github.com>
2021-08-30 22:26:56 -0500

    Add files via upload

---
+++ Q5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Declare variables
+    int cel = 100;
+    int fah = ((9.0/5.0)*(cel)) + 32;
+
+    // Loop to find when the two ints are equal
+    while (fah != cel){
+       cel--;
+       fah = ((9.0/5.0)*(cel)) + 32;
+    }
+
+    cout << "The temperature where the value in Celsius and Fahrenheit is the same is " << fah << " degrees.";
+
+    return 0;
+
+}
